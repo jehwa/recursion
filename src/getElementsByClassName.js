9486d2d76cfc9086e33d839bc96cf7f480a07262
Jehwa Shin <s.jehwa@gmail.com>
2018-04-11 15:53:57 -0700

    minimum requirements done

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
-};
+  let output = [];
+
+  function findClass(nodes) {
+    if(nodes.classList && nodes.classList.contains(className)) {
+      output.push(nodes);
+    }
+    if(nodes.childNodes) {
+      nodes.childNodes.forEach(function (eachNode) {
+        findClass(eachNode);
+      });
+    }
+    return output;
+  }
+  findClass(document.body);
+  return output;
+}
+
+
+// document.body, element.childNodes, and element.classList
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,41 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+let stringifyJSON = function(obj) {
   // your code goes here
+  if(typeof obj === 'function') {
+    return null;
+  }
+  if(typeof obj === 'undefined') {
+    return null;
+  }
+  if(obj === null) {
+    return 'null';
+  }
+  if(typeof obj === 'string') {
+    return `"${obj}"`;
+  }
+  if(typeof obj === 'number') {
+    return `${obj}`;
+  }
+  if(typeof obj === 'boolean') {
+    return `${obj}`;
+  }
+  if(Array.isArray(obj)) {
+    return '[' +
+    obj.map(function(o) {
+      return stringifyJSON(o)
+    }).join(',') + ']';
+  }
+  if(!Array.isArray(obj) && typeof obj === 'object') {
+    let result = [];
+    Object.keys(obj).forEach(function(key) {
+      let val = stringifyJSON(obj[key]);
+      if(val !== null) {
+        result.push(`"${key}":${val}`);
+      }
+    })
+    return '{' + result.join(',') + '}';
+  }
+
 };
